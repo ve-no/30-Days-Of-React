b86378d89b87986152dda54d8aac223083cf72c6
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-07-17 22:20:32 +0100

    first commit

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,154 @@
+ 
+
+// ### 💻 Exercise
+
+// ##### Exercise: Level 1
+
+// ```js
+// const countries = [
+//   'Albania',
+//   'Bolivia',
+//   'Canada',
+//   'Denmark',
+//   'Ethiopia',
+//   'Finland',
+//   'Germany',
+//   'Hungary',
+//   'Ireland',
+//   'Japan',
+//   'Kenya',
+// ]
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB',
+// ]
+// ```
+
+// 1. Declare an _empty_ array;
+// 2. Declare an array with more than 5 number of elements
+// 3. Find the length of your array
+// 4. Get the first item, the middle item and the last item of the array
+// 5. Declare an array called _mixedDataTypes_, put different data types in the array and find the length of the array. The array size should be greater than 5
+// 6. Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+// 7. Print the array using _console.log()_
+// 8. Print the number of companies in the array
+// 9. Print the first company, middle and last company
+// 10. Print out each company
+// 11. Change each company name to uppercase one by one and print them out
+// 12. Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+// 13. Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is _not found_
+// 14. Filter out companies which have more than one 'o' without the filter method
+// 15. Sort the array using _sort()_ method
+// 16. Reverse the array using _reverse()_ method
+// 17. Slice out the first 3 companies from the array
+// 18. Slice out the last 3 companies from the array
+// 19. Slice out the middle IT company or companies from the array
+// 20. Remove the first IT company from the array
+// 21. Remove the middle IT company or companies from the array
+// 22. Remove the last IT company from the array
+// 23. Remove all IT companies
+
+// 1. Declare an _empty_ array;
+const emptyArray = []; 
+
+// 2. Declare an array with more than 5 number of elements
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// 3. Find the length of your array
+// console.log(array.length);
+
+// 4. Get the first item, the middle item and the last item of the array
+// console.log(array[0], array[Math.trunc(array.length / 2)], array[array.length - 1]);
+
+// 5. Declare an array called _mixedDataTypes_, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = [1, 'data', 1.5, [1, 'data'], {name : 'allal', age : 26}, true, ()=>{return 5}];
+// console.log(typeof(mixedDataTypes[6]));
+
+// 6. Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+let itCompanies = [];
+itCompanies.push("faceboofk", "Google", "Microsoft", "Apple", "IBM", "oracle", "amazon")
+
+
+// 7. Print the array using _console.log()_
+// console.log(itCompanies)
+
+// 8. Print the number of companies in the array
+// console.log(itCompanies.length);
+
+// 9. Print the first company, middle and last company
+// console.log(itCompanies[0], itCompanies[Math.trunc(itCompanies.length / 2)], itCompanies[itCompanies.length - 1]);
+
+// 10. Print out each company
+// itCompanies.forEach(function(company) {
+  // console.log(company);});
+
+// 11. Change each company name to uppercase one by one and print them out
+// itCompanies.forEach(function(company) {
+  // console.log(company.toUpperCase());});
+
+// 12. Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+// itCompanies[itCompanies.length - 1] = "and Amazon are big IT companies"
+// itCompanies.forEach(function (company, index) {
+//  (itCompanies.length -1 === index || itCompanies.length -2 === index) ?
+//  itCompanies[index] = company.concat(" ") : itCompanies[index] = company.concat(", ")
+// });
+// console.log(itCompanies.join(""));
+
+// 13. Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is _not found_
+// itCompanies.forEach(function(company) {
+//   console.log(company ? company : "is _not found"); 
+// })
+
+// 14. Filter out companies which have more than one 'o' without the filter method
+// let i = itCompanies.length - 1, companies = [], index = 0;
+// while (i >= 0) {
+//   let j, k;
+//   j =  itCompanies[i].length - 1;
+//   k = 0;
+//   while (j >= 0)
+//   {
+//       if (itCompanies[i][j] === 'o')
+//           k++;
+//       j--;
+//   }
+//   if (k < 2)
+//      companies[index]= itCompanies[i]
+//  i--;
+//  index++;
+ 
+// }
+// console.log(companies);
+
+// 15. Sort the array using _sort()_ method
+itCompanies.sort();
+// console.log(itCompanies);
+
+// 16. Reverse the array using _reverse()_ method
+itCompanies.reverse();
+
+// 17. Slice out the first 3 companies from the array
+// console.log(itCompanies.slice(0, 3));
+
+// 20. Remove the first IT company from the array
+// itCompanies.shift()
+// console.log(itCompanies);
+
+// 21. Remove the middle IT company or companies from the array
+let j = Math.trunc(itCompanies.length / 2)
+
+// itCompanies.splice(j, 1)
+// console.log(itCompanies);
+
+// 22. Remove the last IT company from the array
+// itCompanies.pop()
+// console.log(itCompanies);
+
+// 23. Remove all IT companies
+itCompanies.splice(0);
+console.log(itCompanies);
---
+++ solutions/day-01/level2.js
@@ -0,0 +1,111 @@
+// ##### Exercise: Level 2
+
+// 1. Create a separate countries.js file and store the countries array into this file, create a separate file web_techs.js and store the webTechs array into this file. Access both file in main.js file
+// 1. First remove all the punctuations and change the string to array and count the number of words in the array
+
+//    ```js
+//    let text =
+//      'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+//    console.log(words)
+//    console.log(words.length)
+//    ```
+
+//    ```sh
+//    ["I", "love", "teaching", "and", "empowering", "people", "I", "teach", "HTML", "CSS", "JS", "React", "Python"]
+
+//    13
+//    ```
+
+// 1. In the following shopping cart add, remove, edit items
+
+//    ```js
+//    const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//    ```
+
+//    - add 'Meat' in the beginning of your shopping cart if it has not been already added
+//    - add Sugar at the end of you shopping cart if it has not been already added
+//    - remove 'Honey' if you are allergic to honey
+//    - modify Tea to 'Green Tea'
+
+// 1. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+// 1. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+// 1. Concatenate the following two variables and store it in a fullStack variable.
+
+//    ```js
+//    const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+//    const backEnd = ['Node', 'Express', 'MongoDB']
+
+//    console.log(fullStack)
+//    ```
+
+//    ```sh
+//    ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+//    ```
+
+
+// 1. First remove all the punctuations and change the string to array and count the number of words in the array
+
+let text ='I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let spl_txt = text.split(' ')  
+let i = spl_txt.length -1
+while (i >= 0) {
+    let j = spl_txt[i].length - 1;
+   if (spl_txt[i][j] === ',' || spl_txt[i][j] === '.') {
+
+       spl_txt[i] = spl_txt[i].split('');
+       spl_txt[i].splice(-1)
+       spl_txt[i] = spl_txt[i].join('');
+   
+   }
+    i--
+    
+}
+
+// console.log(spl_txt);
+
+// 1. In the following shopping cart add, remove, edit items
+//    const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//    ```
+
+//    1- add 'Meat' in the beginning of your shopping cart if it has not been already added
+//    2- add Sugar at the end of you shopping cart if it has not been already added
+//    3- remove 'Honey' if you are allergic to honey
+//    4- modify Tea to 'Green Tea'
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+shoppingCart.unshift('Meat') // 1
+// 2
+if (shoppingCart[shoppingCart.length - 1] !== 'Sugar') 
+    shoppingCart.push('Sugar') // 2
+// 3
+shoppingCart.splice(-2, 1)
+// 4
+shoppingCart[3] = 'Green Tea'
+// console.log(shoppingCart);
+
+// 1. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+// 1. Concatenate the following two variables and store it in a fullStack variable.
+
+//    ```js
+//    const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+//    const backEnd = ['Node', 'Express', 'MongoDB']
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'  ]
+const backEnd = ['Node', 'Express', 'MongoDB']
+let ind = frontEnd.length - 1, m = 0;
+    while (ind >= 0) 
+    {
+        if (frontEnd[ind] === 'Sass')
+        {
+            console.log('Sass is a CSS preprocess'); 
+            m++;
+        }
+        ind--
+    }
+    if (m === 0)
+    {
+        frontEnd.push('Sass');
+        console.log(frontEnd);
+    }
+    const fullStack = frontEnd.concat(backEnd);
+    console.log(fullStack);
+
+
