bc23e5aca298c1b4cfe7470556a16f8971b7ec2b
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-07-31 21:10:12 +0100

    just a commit

--- solutions/day-05/src/HexaColor.jsx
+++ solutions/day-05/src/HexaColor.jsx
@@ -1,7 +1,8 @@
 const HexaColor = () => {
 	const rand =()=> '#'+Math.floor(Math.random()*16777215).toString(16)
 	const hexa = new Array(32).fill(0)
-	const heading = hexa.map(() => <h1 style={{backgroundColor: rand()}}>{rand()}</h1>)
+	const heading = hexa.map(() =>
+					<h1 style={{backgroundColor: rand()}}>{rand()}</h1>)
 	return <div className="HexaColor">
 				<h1>30 Days Of React</h1>
 				<h2>Hexadecimal Generator</h2>
