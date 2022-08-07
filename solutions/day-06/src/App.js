372ba08cfbd139a8502552455090c0bbb0c968aa
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-07 21:59:44 +0100

    just a commit

---
+++ solutions/day-06/src/App.js
@@ -0,0 +1,24 @@
+// index.js
+import React from 'react'
+// import ReactDOM from 'react-dom'
+class App extends React.Component {
+  // declaring state
+  state = {
+    count: 0,
+  }
+  render() {
+    // accessing the state value
+    const count = this.state.count
+    return (
+      <div className='App'>
+        <h1>{count} </h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Add One
+        </button>
+      </div>
+    )
+  }
+}
+export default App
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<App />, rootElement)
